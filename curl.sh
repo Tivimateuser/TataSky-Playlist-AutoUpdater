d640d7aeffbc2e9d18010028c7536450e7ecf7ce
Tivimateuser <96577996+Tivimateuser@users.noreply.github.com>
2022-02-27 10:22:42 +0530

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 if [[ $OSTYPE == 'linux-android'* ]]; then
-pkg install git; git clone https://github.com/Tivimate/TataSky-Playlist-AutoUpdater; cd TataSky-Playlist-AutoUpdater; bash main.sh
+pkg install git; git clone https://github.com/Tivimateuser/TataSky-Playlist-AutoUpdater; cd TataSky-Playlist-AutoUpdater; bash main.sh
 elif [[ $OSTYPE == 'linux-gnu'* ]]; then
-git clone https://github.com/Tivimate/TataSky-Playlist-AutoUpdater; cd TataSky-Playlist-AutoUpdater; bash main.sh
+git clone https://github.com/Tivimateuser/TataSky-Playlist-AutoUpdater; cd TataSky-Playlist-AutoUpdater; bash main.sh
 fi
